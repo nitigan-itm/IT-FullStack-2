d569d13f0d98a63f8d34052d514427a27a3fba85
nitigan-itm <nitigan.n@itm.kmutnb.ac.th>
2026-05-26 11:24:16 +0700

    port3000

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
-//const port = 3000;
-const port = process.env.PORT || 3000;
+const port = 3000;
+//const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
